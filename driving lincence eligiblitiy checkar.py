2171256e0c11ce6dd85b0d4040c5807d905afa6a
Vishawanath kokare <vishkokre@gmail.com>
2020-05-15 10:11:18 +0530

    Add files via upload

---
+++ driving lincence eligiblitiy checkar.py
@@ -0,0 +1,22 @@
+### Enter your age
+name = input("Type your name: ")#input primary get string
+var  = int (input('Enter Your Age: '))
+if(var>17) :
+    print("You eligible for get Drive license\n")
+#this is second if
+if(var>99) :
+    print('In this world very low people can lives on hundred')
+    print('your age is not 100 then you enterd worng number of your age')
+elif(var==17) :
+    print("You eligibal for learning Driving license\n")
+#i think only one of elif can use for it but age toolkit wants many more elief
+#adding secound of elif
+elif(var==16) :
+    print('You eligibal for learning Driving license\n')
+#elese no worry about this is have this drving lincense checker i think is only use else one time
+else:
+   print("You not eligible for get Driving license\n")
+#adding last of print for user
+print(name,"thanks for using")
+print('Thanks for using this toolkit\n')
+print('have a nice day')
